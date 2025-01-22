fe725dd4eb7bb6d69e8d0f5e898ace4346c356b9
Zouhair Fouiguira <zouhairfgra@gmail.com>
2025-01-22 12:00:00 +0100

    feat: wrap RootNavigator in SafeAreaView and add StatusBar; update HomeScreen background color

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DripsyProvider } from 'dripsy';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/RootNavigator';
 import theme from './src/styles/theme';
@@ -7,9 +8,18 @@ import theme from './src/styles/theme';
 export default function App() {
   return (
     <DripsyProvider theme={theme}>
-     
-        <RootNavigator />
-     
+      <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+
+        
+          <RootNavigator />
+      </SafeAreaView>
     </DripsyProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
