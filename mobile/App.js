c33da60330d3f97d39a8e735d66cdf983776a656
Zouhair Fouiguira <zouhairfgra@gmail.com>
2025-01-24 23:41:21 +0100

    feat: enhance HomeScreen and ManagementScreen with new styles and layout; update theme colors and structure

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { DripsyProvider } from 'dripsy';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation/RootNavigator';
-import theme from './src/styles/theme';
+import  theme  from './src/styles/theme';  // Import theme from your theme.js
+import RootNavigator from './src/navigation/RootNavigator'; // Adjust based on your navigator path
 
 export default function App() {
   return (
     <DripsyProvider theme={theme}>
-      <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
-
-        
-          <RootNavigator />
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+        <RootNavigator />
       </SafeAreaView>
     </DripsyProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-});
