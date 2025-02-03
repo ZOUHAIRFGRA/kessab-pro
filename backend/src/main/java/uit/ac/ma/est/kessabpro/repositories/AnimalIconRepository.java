41f3e5d63ef85cb70913c099a25039d55f60a444
lahbouch <lahbouch.dev@gmail.com>
2025-02-03 21:51:31 +0100

    [fix] update icons

--- backend/src/main/java/uit/ac/ma/est/kessabpro/repositories/AnimalIconRepository.java
+++ backend/src/main/java/uit/ac/ma/est/kessabpro/repositories/AnimalIconRepository.java
@@ -3,8 +3,6 @@ package uit.ac.ma.est.kessabpro.repositories;
 import uit.ac.ma.est.kessabpro.models.entities.AnimalIcon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface AnimalIconRepository extends JpaRepository<AnimalIcon, Long> {
 
 }
