6ea3a5b004e7e22236cbfcdf3dcda062b9cdbdf8
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-02-25 16:10:31 +0000

    feat: add Footer and HowItWorks components, enhance Navbar with mobile menu support, and update Home page layout

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,6 +2,11 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import CTA from "@/components/CTA";
+import HowItWorks from "@/components/HowItWorks";
+import Testimonials from "@/components/Testimonials";
+import FAQ from "@/components/FAQ";
+import MeetTheDevelopers from "@/components/MeetTheDevelopers";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
@@ -9,7 +14,13 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Features />
+      <HowItWorks   />
+      <Testimonials />
+      <FAQ />
+      <MeetTheDevelopers />
       <CTA />
+      <Footer />
+
     </div>
   );
 };
