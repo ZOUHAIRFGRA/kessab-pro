ae7ea0ae2045becbeb4c71254921968931b20031
Abdelhamid Lahbouch <lahbouch.dev@gmail.com>
2025-03-01 19:31:01 +0000

    feat: remove ValidateTransactionAmount annotation and its validator; add SaleCreatedEvent and TransactionDeletedEvent classes; implement SaleListener for transaction events; create EventService for application readiness

---
+++ backend/src/main/java/uit/ac/ma/est/kessabpro/services/implementations/AppGlobalEventsService.java
@@ -0,0 +1,34 @@
+package uit.ac.ma.est.kessabpro.services.implementations;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import uit.ac.ma.est.kessabpro.seeders.AnimalSeeder;
+import uit.ac.ma.est.kessabpro.seeders.BuyerSaleTransactionSeeder;
+import uit.ac.ma.est.kessabpro.seeders.UserSeeder;
+
+@Service
+public class EventService {
+
+    private AnimalSeeder animalSeeder;
+     private BuyerSaleTransactionSeeder buyerSaleTransactionSeeder;
+     private UserSeeder userSeeder;
+
+    @Autowired
+    void EventListener(AnimalSeeder animalSeeder , BuyerSaleTransactionSeeder buyerSaleTransactionSeeder , UserSeeder userSeeder){
+            this.animalSeeder = animalSeeder;
+            this.buyerSaleTransactionSeeder = buyerSaleTransactionSeeder;
+            this.userSeeder = userSeeder;
+    }
+
+
+    @EventListener
+    public void onReady(ApplicationReadyEvent event){
+        System.out.println("app ready");
+        animalSeeder.seedData();
+        userSeeder.seedData();
+        buyerSaleTransactionSeeder.seedData();
+    }
+}
