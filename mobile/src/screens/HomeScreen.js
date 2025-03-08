1f3cdb6b9b679c2534f6ebf65f628f8a93d74fe4
Abdelhamid Lahbouch <lahbouch.dev@gmail.com>
2025-03-08 20:57:45 +0000

    feat: Add WeatherWidget to HomeScreen and clean up unused imports in backend

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -72,6 +72,7 @@ export default function HomeScreen() {
   return (
     <Container>
       <ContentContainer>
+        <WeatherWidget />
         <SearchInputContainer>
           <SearchInput
             ref={searchInputRef}
